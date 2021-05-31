f90d2903db833e9e2d8c737dee4d8ab17a2a0aa6
Ankit Anand <67144795+git-for-ankit@users.noreply.github.com>
2021-05-31 16:32:26 +0530

    removing comments

--- microservices/implementation/descovery-service/src/main/java/com/example/microservice/descoveryservice/DescoveryServiceApplication.java
+++ microservices/implementation/descovery-service/src/main/java/com/example/microservice/descoveryservice/DescoveryServiceApplication.java
@@ -9,7 +9,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class DescoveryServiceApplication {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		SpringApplication.run(DescoveryServiceApplication.class, args);
 
 	}
